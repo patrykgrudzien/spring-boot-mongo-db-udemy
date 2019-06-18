6884643db3b1ae5616fb71af1d04774fcce57f48
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2019-06-18 21:21:27 +0200

    Created initial project structure

---
+++ lego-store/src/main/java/com/jurik99/LegoStoreApplication.java
@@ -0,0 +1,13 @@
+package com.jurik99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegoStoreApplication.class, args);
+    }
+
+}
