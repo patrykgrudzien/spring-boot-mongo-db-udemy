80dc8b42aa3964c44ce21d785b479f61f2baf780
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2019-06-21 10:05:47 +0200

    Get All Documents

--- lego-store/src/main/java/com/jurik99/api/LegoStoreController.java
+++ lego-store/src/main/java/com/jurik99/api/LegoStoreController.java
@@ -1,12 +1,15 @@
 package com.jurik99.api;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jurik99.model.LegoSet;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/legostore/api")
@@ -16,6 +19,11 @@ public class LegoStoreController {
 
     public LegoStoreController(final MongoTemplate mongoTemplate) {
         this.mongoTemplate = mongoTemplate;
+    }
+
+    @GetMapping("/all")
+    public Collection<LegoSet> all() {
+        return mongoTemplate.findAll(LegoSet.class);
     }
 
     @PostMapping
