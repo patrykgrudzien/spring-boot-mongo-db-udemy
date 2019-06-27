611c1462aba2a433b42e46369be993b625f0f2d1
patrykgrudzien <patryk.grudzien.91@gmail.com>
2019-06-27 10:30:30 +0200

    Integration testing

---
+++ lego-store/src/test/java/com/jurik99/LegoStoreDatabaseTests.java
@@ -0,0 +1,78 @@
+package com.jurik99;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.jurik99.model.DeliveryInfo;
+import com.jurik99.model.LegoSet;
+import com.jurik99.model.LegoSetDifficulty;
+import com.jurik99.model.ProductReview;
+import com.jurik99.persistence.LegoSetRepository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
+public class LegoStoreDatabaseTests {
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+	@Autowired
+	private LegoSetRepository legoSetRepository;
+
+	@SuppressWarnings("Duplicates")
+	@Before
+	public void before() {
+		// clean-up
+		legoSetRepository.deleteAll();
+
+		final LegoSet millenniumFalcon = new LegoSet(
+				"Millennium Falcon",
+				"Star Wars",
+				LegoSetDifficulty.HARD,
+				new DeliveryInfo(LocalDate.now().plusDays(1), 30, true),
+				Arrays.asList(
+						new ProductReview("Dan", 7),
+						new ProductReview("Anna", 10),
+						new ProductReview("John", 8)
+				)
+		);
+
+		final LegoSet skyPolice = new LegoSet(
+				"Sky Police Air Base",
+				"City",
+				LegoSetDifficulty.MEDIUM,
+				new DeliveryInfo(LocalDate.now().plusDays(3), 50, true),
+				Arrays.asList(
+						new ProductReview("Dan", 5),
+						new ProductReview("Andrew", 8)
+				)
+		);
+
+		legoSetRepository.insert(millenniumFalcon);
+		legoSetRepository.insert(skyPolice);
+	}
+
+    @Test
+    public void findAllByGreatReviewsShouldReturnProductsThatHaveReviewWithRatingOf10() {
+		// when
+	    final List<LegoSet> results = new ArrayList<>(legoSetRepository.findAllByGreatReviews());
+
+	    // then
+	    assertEquals(1, results.size());
+	    assertEquals("Millennium Falcon", results.get(0).getName());
+    }
+
+}
